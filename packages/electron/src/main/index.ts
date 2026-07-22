bd5643938cead2d7c3294ead4712e2387dda5262
lizhiyu022 <lizhiyu022@ke.com>
2026-07-22 18:38:05 +0800

    refactor(electron): rewrite as multi-entry package (./main + ./renderer)

---
+++ packages/electron/src/main/index.ts
@@ -0,0 +1,21 @@
+import * as Sentry from "@sentry/electron/main";
+
+export function init(opts: any): void {
+  Sentry.init(opts);
+}
+export const captureException = Sentry.captureException;
+export const captureMessage = Sentry.captureMessage;
+export const setUser = Sentry.setUser;
+export const setTag = Sentry.setTag;
+export const setContext = Sentry.setContext;
+export const addBreadcrumb = Sentry.addBreadcrumb;
+export const withScope = Sentry.withScope;
+export const flush = Sentry.flush;
+
+export { createBearerTransport } from "@traceability/shared";
+export { startResourceMonitor, sampleResources, getEnvironment } from "./environment.js";
+export type {
+  ElectronEnvironment,
+  ElectronSystemSnapshot,
+  ResourceMonitorOptions,
+} from "./environment.js";
